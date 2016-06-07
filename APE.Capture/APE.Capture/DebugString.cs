ecbf759d4789184f15100f0d01de4c0f878113ff
APEdevelopment <performanceautomation@outlook.com>
2016-06-07 23:04:29 +0100

    Added header to each cs file
    Added header to each cs file

--- APE.Capture/APE.Capture/DebugString.cs
+++ APE.Capture/APE.Capture/DebugString.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿//
+//Copyright 2016 David Beales
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -113,7 +128,6 @@ namespace APE.Capture
                 //sa.lpSecurityDescriptor = IntPtr.Zero;
                 IntPtr sa = IntPtr.Zero;
 
-
                 // Create the event for slot 'DBWIN_BUFFER_READY'
                 m_AckEvent = NM.CreateEvent(sa, false, false, "DBWIN_BUFFER_READY");
                 if (m_AckEvent == IntPtr.Zero)
@@ -195,7 +209,6 @@ namespace APE.Capture
             }
         }
 
-
         /// <summary>
         /// Output the debug string
         /// </summary>
@@ -248,7 +261,6 @@ namespace APE.Capture
                 }
                 m_SharedFile = IntPtr.Zero;
             }
-
 
             // Unmap SharedMem
             if (m_SharedMem != IntPtr.Zero)
