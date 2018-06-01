a6e786b102e61a8b88990ebde9b44033720cba44
APEdevelopment <performanceautomation@outlook.com>
2018-06-01 18:55:22 +0100

    Added results grid

--- APE.Spy/APE.Spy/APE.Spy.cs
+++ APE.Spy/APE.Spy/APE.Spy.cs
@@ -1177,6 +1177,9 @@ namespace APE.Spy
                     case "PictureBox":
                         APEType = "GUIPictureBox";
                         break;
+                    case "LZResultsGrid":
+                        APEType = "GUIAxLZResultsGrid";
+                        break;
                     case "OptionButton":
                         APEType = "GUIRadioButton";
                         break;
